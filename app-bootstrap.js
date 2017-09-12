ed80e96cdd76547346f120cf821ddcb47567c303
Arnau Orriols <dev@arnauorriols.com>
2017-09-12 08:22:56 +0200

    Empty the compat function
    It was an experimental approach, not really needed right now.

--- app-bootstrap.js
+++ app-bootstrap.js
@@ -21,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
 								localModel = localStorage.getItem(STORAGE_KEY);
 								if (!localModel || !localModel.timestamp || (model.timestamp > localModel.timestamp)) {
 									console.log('Model in database is newer than local version. Syncing...');
+									// Firebase does not store empty arrays, we need to manually add them
 									if (!model.hasOwnProperty('jobQueue')) {
 										model.jobQueue = [];
 									}
@@ -88,80 +89,8 @@ document.addEventListener('DOMContentLoaded', function() {
 		};
 
     function compat(model) {
-        for (props of [['thread', 'job'], ['threadQueue', 'jobQueue'], ['newThread', 'newJob']]) {
-            var oldProp = props[0];
-            var newProp = props[1];
-            if (model.hasOwnProperty(oldProp)) {
-                model[newProp] = model[oldProp];
-                delete model[oldProp];
-            }
-        }
-
-				// v.1.2.0
-			  let migrateJob = (job) => {
-						job.title = job.threadName;
-						delete job.threadName;
-						if (job.worklog) {
-								job.journal.unshift(job.worklog);
-						}
-						job.worklog = job.journal;
-						delete job.journal;
-						return job;
-				}
-
-        for (props of [['newJob', 'unsavedJob']]) {
-            var oldProp = props[0];
-            var newProp = props[1];
-            if (model.hasOwnProperty(oldProp)) {
-                model[newProp] = model[oldProp];
-                delete model[oldProp];
-            }
-        }
-
-			  if (model.hasOwnProperty('unsavedJob')) {
-					delete model['usavedJob'];
-				}
-
-				if (model.hasOwnProperty('job')) {
-						if (model.job) {
-							  model.jobQueue.unshift(model.job);
-						}
-					  delete model.job;
-				}
-
-				if (model.jobQueue.length && model.jobQueue[0].hasOwnProperty('threadName')) {
-					  model.jobQueue = model.jobQueue.map(migrateJob);
-				}
-
-				if (model.jobQueue.length && !(model.jobQueue[0] instanceof Array)) {
-					  model.jobQueue = model.jobQueue.map((job) => ["Queued", job]);
-				}
-
-			  if (model.hasOwnProperty('hotkeysPressed')) {
-						delete model.hotkeysPressed;
-				}
-
-				// v1.8.3
-				if (model.jobQueue.length && (model.jobQueue[0] instanceof Array)) {
-					  model.jobQueue = model.jobQueue.map((job) => job[1]);
-				}
-
-				if (!model.hasOwnProperty('nextJobStatus')) {
-					model.nextJobStatus = 'Queued';
-				}
-
-				model.jobQueue.forEach(function (jobQueueEntry) {
-					if (!jobQueueEntry.hasOwnProperty('history')) {
-						jobQueueEntry.data = jobQueueEntry;
-						jobQueueEntry.history = {
-							events: [],
-							metricsMsg: {
-								msg: 'NoOp'
-							}
-						};
-					}
-				});
-        return model;
+			// Mutate model to migrate to the newest schema required
+			return model
     }
 		var STORAGE_KEY = 'MultitaskOS-Model';
 		storageContent = localStorage.getItem(STORAGE_KEY);
