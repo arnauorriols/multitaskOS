c01ee584df4b8fde9497e9c29c2d8f0edf754e1e
Arnau Orriols <dev@arnauorriols.com>
2017-09-13 08:56:42 +0200

    Remove docs from chrome-extension distribution

--- bin/build-chrome-extension.sh
+++ bin/build-chrome-extension.sh
@@ -2,4 +2,4 @@ CHROME_EXTENSION_DIR='chrome-extension'
 
 REPOSITORY_ROOT=$(git rev-parse --show-toplevel)
 
-(cd $REPOSITORY_ROOT/$CHROME_EXTENSION_DIR && find . -maxdepth 1 -type l -delete && ln -s ../* . && rm -rf $CHROME_EXTENSION_DIR && rm -rf dist/ && mkdir dist && cd .. && zip -r $CHROME_EXTENSION_DIR/dist/multitaskos.zip $CHROME_EXTENSION_DIR)
+(cd $REPOSITORY_ROOT/$CHROME_EXTENSION_DIR && find . -maxdepth 1 -type l -delete && ln -s ../* . && rm -rf $CHROME_EXTENSION_DIR && rm -rf docs && rm -rf dist/ && mkdir dist && cd .. && zip -r $CHROME_EXTENSION_DIR/dist/multitaskos.zip $CHROME_EXTENSION_DIR)
